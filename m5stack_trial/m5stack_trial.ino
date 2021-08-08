fefa816df513a518f72ea27feda8bcf53734fde9
Tomohiro Harada <haradatmo2@live.jp>
2021-08-09 05:55:32 +0900

    Add IMU initialize

--- m5stack_trial/m5stack_trial.ino
+++ m5stack_trial/m5stack_trial.ino
@@ -1,11 +1,30 @@
 #include <M5Stack.h>
+#include "utility/MPU9250.h"
+
+MPU9250 IMU; // new a MPU9250 object
 
 void setup() {
   M5.begin();
+  Wire.begin();
 
   M5.Power.begin();
+  M5.Lcd.print("[Initialize] Initializing M5Stack...");
 
-  M5.Lcd.print("Hello world");
+  IMU.initMPU9250();
+  M5.Lcd.print("[Initialize] Initializing MPU9250...");
+
+  IMU.initAK8963(IMU.magCalibration);
+  M5.Lcd.print("[Initialize] Intializing AK8963...");
+  if (Serial)
+  {
+    M5.Lcd.println("Calibration values: ");
+    M5.Lcd.print("X-Axis sensitivity adjustment value ");
+    M5.Lcd.println(IMU.magCalibration[0], 2);
+    M5.Lcd.print("Y-Axis sensitivity adjustment value ");
+    M5.Lcd.println(IMU.magCalibration[1], 2);
+    M5.Lcd.print("Z-Axis sensitivity adjustment value ");
+    M5.Lcd.println(IMU.magCalibration[2], 2);
+  }
 
 }
 
